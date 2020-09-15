734cdef2267310f00a91d5b6def46b11a1a0be9f
soumyas567 <soumyas567@gmail.com>
2020-09-15 22:03:47 +0100

    String Exercises programs added

---
+++ DAY  9/Q4.py
@@ -0,0 +1,9 @@
+def replace_char(original_string, o_char, r_char):
+    replaced_string = original_string.replace(o_char, r_char)
+    return replaced_string
+
+
+sample_string = input("Enter the string")
+original = input("Enter the character to be replaced")
+replacement = input("Enter the replacement character")
+print(replace_char(sample_string, original, replacement))
