f1b5cb4831540dc6109b18ccf9bfd6a039068146
soumyas567 <soumyas567@gmail.com>
2020-09-15 22:17:58 +0100

    String Exercises programs added

--- DAY  9/Q3.py
+++ DAY  9/Q3.py
@@ -2,5 +2,5 @@ def first_half(even_string):
     return even_string[:int((len(even_string)) / 2)]
 
 
-even_string = "TextBook"
-print(first_half(even_string))
+even_string = input("Enter the String: ")
+print("First half is ", first_half(even_string))
--- DAY  9/Q4.py
+++ DAY  9/Q4.py
@@ -3,7 +3,7 @@ def replace_char(original_string, o_char, r_char):
     return replaced_string
 
 
-sample_string = input("Enter the string")
-original = input("Enter the character to be replaced")
-replacement = input("Enter the replacement character")
+sample_string = input("Enter the string:")
+original = input("Enter the character to be replaced: ")
+replacement = input("Enter the replacement character: ")
 print(replace_char(sample_string, original, replacement))
