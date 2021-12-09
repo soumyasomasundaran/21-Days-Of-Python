5428820a7cf50d4cbf750e0d503a31d5251c9d12
Soumya <14910227+soumyas567@users.noreply.github.com>
2021-12-09 22:09:40 +0000

    Update area_rect.py

--- DAY1/area_rect.py
+++ DAY1/area_rect.py
@@ -1,5 +1,5 @@
 #Area of rectange
-l=int(input("Enter length"))
-b=int(input("Enter breadth"))
-area=l*b
-print("area is",area)
+l = int(input("Enter length"))
+b = int(input("Enter breadth"))
+area = l * b
+print("area is", area)
