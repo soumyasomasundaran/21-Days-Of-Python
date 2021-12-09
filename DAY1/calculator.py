6bae4d95e2ce858d65acdf9f978de2172ec8c5f5
Soumya <14910227+soumyas567@users.noreply.github.com>
2021-12-09 22:08:51 +0000

    Update calculator.py

--- DAY1/calculator.py
+++ DAY1/calculator.py
@@ -1,11 +1,12 @@
-#Calculator
-a=int(input("Enter the first number"))
-b=int(input("Enter the second nummber"))
-sum=a+b
-difference=a-b
-product=a*b
-quotient=a/b
-print("sum of a and bis ",sum)
+# Calculator Program
+
+a = int(input("Enter the first number"))
+b = int(input("Enter the second nummber"))
+add = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+print("sum of a and b is ",add)
 print("difference of a and b is ",difference)
 print("product of a and b  is ",product)
 print("quotient of a and b is ",quotient)
